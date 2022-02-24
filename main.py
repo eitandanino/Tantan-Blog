d2067771619d43049f596184aa317ea164ae190a
eitandanino <daninoeitan10@gmail.com>
2022-02-24 16:50:48 +0200

    Add psycopg2-binary

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from datetime import date
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
